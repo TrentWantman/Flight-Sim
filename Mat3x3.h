f5cea9fb522958a4885fa97cd39ed45607ce0f30
TrentWantman <wantmantrent@gmail.com>
2026-04-04 17:00:12 -0400

    Print, rotate, identity included in mat3x3. Default constructor typo fixed

--- Mat3x3.h
+++ Mat3x3.h
@@ -11,7 +11,7 @@ private:
 
 public:
 
-    Mat3x3() : m({1, 0, 0, 0, 1, 0, 0, 0, 1}){}
+    Mat3x3() : m{1, 0, 0, 0, 1, 0, 0, 0, 1} {}
 
     Mat3x3(float m0, float m1, float m2, float m3, float m4, float m5, float m6, float m7, float m8)
     : m{m0, m1, m2, m3, m4, m5, m6, m7, m8} {}
@@ -22,10 +22,6 @@ public:
             m[1], m[4], m[7],
             m[2], m[5], m[8]
         );
-    }
-
-    Mat3x3 Identity(){
-        
     }
 
     float Determinant(){
@@ -51,18 +47,43 @@ public:
         );
     }
 
-    Vec3 RotateX(float angle){
-
+    static Mat3x3 Identity(){
+        return Mat3x3();
     }
 
-    Vec3 RotateY(float angle){
-
+    static Mat3x3 RotateX(float angle){
+        float rad = angle * M_PI / 180.0f;
+        return Mat3x3(
+            1, 0, 0,
+            0, cos(rad), -sin(rad),
+            0, sin(rad), cos(rad)
+        );
     }
 
-    Vec3 RotateZ(float angle){
-
+    static Mat3x3 RotateY(float angle){
+        float rad = angle * M_PI / 180.0f;
+        return Mat3x3(
+            cos(rad), 0, sin(rad),
+            0, 1, 0,
+            -sin(rad), 0, cos(rad)
+        );
     }
 
+    static Mat3x3 RotateZ(float angle){
+        float rad = angle * M_PI / 180.0f;
+        return Mat3x3(
+            cos(rad), -sin(rad), 0,
+            sin(rad), cos(rad), 0,
+            0, 0, 1
+
+        );
+    }
+
+    void print(){
+        printf("%f %f %f\n", m[0], m[1], m[2]);
+        printf("%f %f %f\n", m[3], m[4], m[5]);
+        printf("%f %f %f\n", m[6], m[7], m[8]);
+    }   
 
 };
 #endif
