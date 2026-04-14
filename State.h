f717f9a3c873a04fb06a63fcdee1d171f87a802d
TrentWantman <wantmantrent@gmail.com>
2026-04-13 22:42:50 -0400

    Operator overloads for state

--- State.h
+++ State.h
@@ -6,4 +6,16 @@
 //[px, py, pz, vx, vy, vz, mass] rocker state vector
 using State = std::array<float, 7>;
 
+inline State operator+(const State& state, const State& b) {
+    State result;
+    for (size_t i = 0; i < state.size(); i++) result[i] = state[i] + b[i];
+    return result;
+}
+
+inline State operator*(const State& state, float s) {
+    State result;
+    for (size_t i = 0; i < state.size(); i++) result[i] = state[i] * s;
+    return result;
+}
+
 #endif
